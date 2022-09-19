f2a3ed584255395c79839fbc33ea63093f099e1a
Anatoliy K <778966@mail.ru>
2022-09-19 09:39:21 +0500

    added task3

---
+++ Example03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Нпиши первое число: ");
+float user_number1 = float.Parse(Console.ReadLine());
+
+if (user_number1 % 2 == 0)
+{
+    Console.Write("Число " + user_number1  + " четное");
+} 
+else
+{
+    Console.Write("Число  " + user_number1 + " нечетное");
+}
