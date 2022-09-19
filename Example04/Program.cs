42711008d7b643c521ea505a6b837369d4e24d60
Anatoliy K <778966@mail.ru>
2022-09-19 10:18:39 +0500

    added task4

---
+++ Example04/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Нпиши число: ");
+float user_number = float.Parse(Console.ReadLine());
+float index = 0;
+while (index <= user_number)
+{
+    if (index % 2 == 0)
+    { 
+        Console.Write(index + ", ");
+        index++;
+    }
+    else
+    {
+        index++;
+    }
+
+}
+
