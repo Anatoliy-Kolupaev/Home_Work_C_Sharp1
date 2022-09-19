8d173e91a59e21d36a34f001497ee93d2d41e38b
Anatoliy K <778966@mail.ru>
2022-09-19 22:20:00 +0500

    added ex1

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -5,8 +5,10 @@ int user_number2 = int.Parse(Console.ReadLine());
 if (user_number1 > user_number2)
 {
     Console.WriteLine("Max = " + user_number1);
+    Console.WriteLine("Min = " + user_number2);
 }
 else
 {
     Console.WriteLine("Max = " + user_number2);
+    Console.WriteLine("Min = " + user_number1);
 }
